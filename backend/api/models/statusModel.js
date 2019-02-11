b5b51436152b7bce3834bffaaea02c65dbb331f5
cwlowder <no>
2019-02-11 02:11:07 -0600

    Tabs > Spaces

--- backend/api/models/statusModel.js
+++ backend/api/models/statusModel.js
@@ -4,19 +4,19 @@ var Schema = mongoose.Schema;
 
 
 var TaskSchema = new Schema({
-  name: {
-    type: String,
-    required: 'The name of the section'
-  },
-  Created_date: {
-    type: Date,
-    default: Date.now
-  },
-  status: {
-    type: String,
-    enum: ['closed', 'dirty', 'closing', 'open'],
-    default: 'open'
-  }
+	name: {
+		type: String,
+		required: 'The name of the section'
+	},
+	Created_date: {
+		type: Date,
+		default: Date.now
+	},
+	status: {
+		type: String,
+		enum: ['closed', 'dirty', 'closing', 'open'],
+		default: 'open'
+	}
 });
 
 module.exports = mongoose.model('status', TaskSchema);
--- backend/api/routes/statusRoutes.js
+++ backend/api/routes/statusRoutes.js
@@ -1,17 +1,17 @@
 'use strict';
 module.exports = function(app) {
-  var status = require('../controllers/statusController');
+	var status = require('../controllers/statusController');
 
-  // todoList Routes
-  app.route('/status')
-    .get(status.list_status)
-    .post(status.create_a_status)
-    .head(status.create_general_status);
+	// todoList Routes
+	app.route('/api/status')
+		.get(status.list_status)
+		.post(status.create_a_status)
+		.head(status.create_general_status);
 
 
-  app.route('/status/:sectionId')
-    .get(status.read_a_status)
-    .post(status.update_a_status)
-    .delete(status.delete_a_status);
+	app.route('/api/status/:sectionId')
+		.get(status.read_a_status)
+		.post(status.update_a_status)
+		.delete(status.delete_a_status);
 };
 
