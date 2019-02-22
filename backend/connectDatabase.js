d93d8296f8d0f922a8e708cfbec8ab43b94cba49
cwlowder <no>
2019-02-21 22:05:06 -0600

    Added tests

--- backend/connectDatabase.js
+++ backend/connectDatabase.js
@@ -4,6 +4,11 @@ mongoose.Promise = global.Promise;
 // connects mongoose to the correct database depending on the enviornment context
 // Returns the disconnect function
 module.exports = function(opts={useNewUrlParser: true}) {  
+  // Check not already connected
+  if (mongoose.connection.readyState === 1 || mongoose.connection.readyState === 2) {
+    throw new Error('Mongoose is already connected:' + mongoose.connection.readyState)
+  }
+
   var password = process.env.MONGODB_URI 
   var uname = process.env.MONGODB_UNAME // pedcosmosdb
   var host = process.env.MONGODB_HOST // pedcosmosdb.documents.azure.com:10250/pedway?ssl=true
@@ -18,5 +23,6 @@ module.exports = function(opts={useNewUrlParser: true}) {
     mongoose.connect('mongodb://'+uname+':'+password+'@'+host, opts);
   }
 
-  return () => mongoose.connection.close();
+  // Returns a function that closes the database and returns a promise
+  return () => {return mongoose.connection.close()};
 }
