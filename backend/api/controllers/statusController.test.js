87d8021b832e86e5449ecf662f4a3f80c0a5112f
Alex Vanyo <vanyoalex@gmail.com>
2019-02-25 19:35:06 -0600

    Updated backend to have correct styling

--- backend/api/controllers/statusController.test.js
+++ backend/api/controllers/statusController.test.js
@@ -1,12 +1,12 @@
 const request = require('supertest');
 const {app, disconnect} = require('../../app');
-const {delete_all} = require('./statusController')
+const {deleteAll} = require('./statusController');
 
-afterAll(disconnect)
+afterAll(disconnect);
 
 describe('Test the root of the status api', () => {
-  beforeEach(done=>{
-    delete_all(done)
+  beforeEach((done) => {
+    deleteAll(done);
   });
 
   test('it should respond to the GET method', (done) => {
@@ -31,87 +31,92 @@ describe('Test the root of the status api', () => {
   });
 
   test('it should respond to the POST method', (done) => {
-    request(app).post('/api/status').send({
-        name: "Test Section"
-      }).then((response) => {
-      expect(response.statusCode).toBe(200);
-      expect(typeof response.body).toBe('object');
-      expect(response.body['name']).toBe('Test Section');
-      expect(response.body['status']).toBe('open');
+    request(app)
+        .post('/api/status')
+        .send({name: 'Test Section'})
+        .then((response) => {
+          expect(response.statusCode).toBe(200);
+          expect(typeof response.body).toBe('object');
+          expect(response.body['name']).toBe('Test Section');
+          expect(response.body['status']).toBe('open');
 
-      var id = response.body['_id'];
+          const id = response.body['_id'];
 
-      request(app).get('/api/status').then((response) => {
-        expect(response.statusCode).toBe(200);
-        expect(response.body.length).toBe(1);
-        expect(response.body[0]['_id']).toBe(id);
-        expect(response.body[0]['name']).toBe('Test Section');
-        expect(response.body[0]['status']).toBe('open');
-      done();
-      });
-    });
+          request(app).get('/api/status').then((response) => {
+            expect(response.statusCode).toBe(200);
+            expect(response.body.length).toBe(1);
+            expect(response.body[0]['_id']).toBe(id);
+            expect(response.body[0]['name']).toBe('Test Section');
+            expect(response.body[0]['status']).toBe('open');
+            done();
+          });
+        });
   });
 });
 
 describe('Test the specific sections of the status api', () => {
-  var id;
-  beforeEach(done=>{
+  let id;
+  beforeEach((done) => {
     // Clear data and Upload test status to the API
-    delete_all(()=>{
-      request(app).post('/api/status').send({
-        name: "Test Section"
-      }).then((response)=>{
-        id = response.body["_id"];
-        done()
-      });
+    deleteAll(() => {
+      request(app)
+          .post('/api/status')
+          .send({name: 'Test Section'})
+          .then((response) => {
+            id = response.body['_id'];
+            done();
+          });
     });
   });
 
   test('it should respond to the GET method', (done) => {
-    request(app).get('/api/status/'+id).then((response) => {
+    request(app).get('/api/status/' + id).then((response) => {
       expect(response.statusCode).toBe(200);
       expect(typeof response.body).toBe('object');
-      expect(response.body['name']).toBe("Test Section");
+      expect(response.body['name']).toBe('Test Section');
       expect(response.body['_id']).toBe(id);
       done();
     });
   });
 
   test('it should respond to the POST method', (done) => {
-    request(app).post('/api/status/'+id).send({
-      name: "New Name",
-      status: "closed"
-    }).then((response) => {
-      expect(response.statusCode).toBe(200);
-      request(app).get('/api/status').then((response) => {
-        expect(response.body.length > 0).toBe(true);
-        expect(response.body[0]['_id']).toBe(id);
-        expect(response.body[0]['name']).toBe("New Name");
-        expect(response.body[0]['status']).toBe('closed');
-
-        // Check if the database has been updated
-        request(app).get('/api/status/'+id).then((response) => {
+    request(app)
+        .post('/api/status/' + id)
+        .send({name: 'New Name', status: 'closed'})
+        .then((response) => {
           expect(response.statusCode).toBe(200);
-          expect(typeof response.body).toBe('object');
-          expect(response.body['name']).toBe("New Name");
-          expect(response.body['_id']).toBe(id);
-          done();
+          request(app).get('/api/status').then((response) => {
+            expect(response.body.length > 0).toBe(true);
+            expect(response.body[0]['_id']).toBe(id);
+            expect(response.body[0]['name']).toBe('New Name');
+            expect(response.body[0]['status']).toBe('closed');
+
+            // Check if the database has been updated
+            request(app).get('/api/status/' + id).then((response) => {
+              expect(response.statusCode).toBe(200);
+              expect(typeof response.body).toBe('object');
+              expect(response.body['name']).toBe('New Name');
+              expect(response.body['_id']).toBe(id);
+              done();
+            });
+          });
         });
-      });
-    });
   });
 
-  test('it should respond to the DELETE methid', (done) => {
-    request(app).delete('/api/status/'+id).then((response) => {
-      expect(response.statusCode).toBe(200);
-      expect(response.body['message']).toBe("Section Status successfully deleted");
+  test(
+      'it should respond to the DELETE methid',
+      (done) => {
+        request(app).delete('/api/status/' + id).then((response) => {
+          expect(response.statusCode).toBe(200);
+          expect(response.body['message'])
+              .toBe('Section Status successfully deleted');
 
-      // Check if the database has been updated
-      request(app).get('/api/status').then((response) => {
-        expect(response.statusCode).toBe(200);
-        expect(response.body.length).toBe(0);
-        done();
+          // Check if the database has been updated
+          request(app).get('/api/status').then((response) => {
+            expect(response.statusCode).toBe(200);
+            expect(response.body.length).toBe(0);
+            done();
+          });
+        });
       });
-    })
-  })
 });
--- backend/connectDatabase.test.js
+++ backend/connectDatabase.test.js
@@ -1,37 +1,37 @@
-var connectDatabase = require('./connectDatabase');
+const connectDatabase = require('./connectDatabase');
 mongoose = require('mongoose');
 
 describe('Test the database connection', () => {
-	test('it should connect and disconnect to the database', (done) => {
-    var disconnect = connectDatabase();
-    disconnect().then(()=>{
+  test('it should connect and disconnect to the database', (done) => {
+    const disconnect = connectDatabase();
+    disconnect().then(() => {
       expect(mongoose.connection.readyState).toBe(0);
       done();
     });
   });
 
   test('it should connect and disconnect multiple times', (done) => {
-    var disconnect = connectDatabase();
-    disconnect().then(()=>{
+    const disconnect = connectDatabase();
+    disconnect().then(() => {
       expect(mongoose.connection.readyState).toBe(0);
-      var disconnect = connectDatabase();
-      disconnect().then(()=>{
+      const disconnect = connectDatabase();
+      disconnect().then(() => {
         expect(mongoose.connection.readyState).toBe(0);
-        var disconnect = connectDatabase();
+        const disconnect = connectDatabase();
         disconnect().then(done);
       });
     });
   });
 
   test('it should not connect twice', (done) => {
-    var disconnect = connectDatabase();
+    const disconnect = connectDatabase();
     expect(connectDatabase).toThrow(Error);
     disconnect().then(done);
   });
 
   test('it should discconnect twice', (done) => {
-    var disconnect = connectDatabase();
-    disconnect().then(()=>{
+    const disconnect = connectDatabase();
+    disconnect().then(() => {
       disconnect().then(done);
     });
   });
