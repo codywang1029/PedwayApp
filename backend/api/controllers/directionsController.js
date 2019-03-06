8bd59b1c428b8bbcb4c3b379a37f9ba52fb615d6
Alex Vanyo <vanyoalex@gmail.com>
2019-03-06 02:18:25 +0000

    Moved directions to ors endpoint and made new request

--- backend/api/controllers/directionsController.js
+++ backend/api/controllers/directionsController.js
@@ -3,15 +3,20 @@
 const ORS_DIRECTION_URL = 'https://api.openrouteservice.org/directions';
 
 const url = require('url');
+const https = require('https');
 
 exports.directions = function(req, res) {
-  res.redirect(url.format({
+  https.request(url.format({
     pathname: ORS_DIRECTION_URL,
     query: {
       ...req.query,
       ...{
         'api_key': process.env.ORS_API_KEY,
-      }
+      },
     }
-  }))
+  }), function(response) {
+    response.pipe(res);
+  }).on('error', function(e) {
+    res.sendStatus(500);
+  }).end();
 };
--- backend/api/routes/directionsRoutes.js
+++ backend/api/routes/directionsRoutes.js
@@ -3,5 +3,5 @@ module.exports = function(app) {
   const directions = require('../controllers/directionsController');
 
   // todoList Routes
-  app.route('/api/directions').all(directions.directions)
+  app.route('/api/ors/directions').all(directions.directions)
 };
