98dd3f55c4b2962dea7604c1010127e5fbd075d3
Kexiang <codywang1029@outlook.com>
2019-03-07 20:34:13 -0600

    refactored map and search bar

--- pedwayApp/App.js
+++ pedwayApp/App.js
@@ -15,13 +15,13 @@
  */
 
 import React, {Component} from 'react';
-import MapView, {MAP_TYPES, UrlTile} from 'react-native-maps';
 import {Button} from 'react-native';
 import {Platform, StyleSheet, Text, View, TouchableOpacity} from 'react-native';
-import Icon from 'react-native-vector-icons/FontAwesome';
 import SideMenu from 'react-native-side-menu';
 import styles from './styles/app.style';
 import RoundButton from './components/RoundButton/RoundButton';
+import GroundMapView from './components/GroundMapView/GroundMapView'
+import SearchBar from './components/SearchBar/SearchBar';
 
 /**
  * HomeScreen that gets rendered first when everything is loaded
@@ -75,18 +75,13 @@ class MainView extends React.Component {
   render() {
     return (
       <View style={{flex: 1}}>
-        <GroundMapView/>
-        <TouchableOpacity
-          style={[styles.floating, styles.searchBox]}
-        >
-          <View style={{
-            flexGrow: 1,
-            justifyContent: 'center',
-            alignItems: 'flex-start',
-          }}>
-            <Text style={{fontSize: 18}}>Enter your destination...</Text>
-          </View>
-        </TouchableOpacity>
+        <GroundMapView region={{
+          latitude: 40.113918,
+          longitude: -88.224916,
+          latitudeDelta: 0.1,
+          longitudeDelta: 0.1,
+        }}/>
+        <SearchBar/>
         <RoundButton style={[positions.undergroundButton]} icon={"level-down"} func={() => {
           // this.props.navigation.dispatch(StackActions.reset({
           //   index: 0,
@@ -100,46 +95,6 @@ class MainView extends React.Component {
   }
 }
 
-
-/**
- * Renders a MapView that display the ground level map
- * we are setting provider to null and UrlTile to OpenStreetMap's API
- * to use OSM
- */
-class GroundMapView extends React.Component {
-
-  constructor() {
-    super();
-    this.state = {
-      apiServerURL: 'http://a.tile.openstreetmap.de/tiles/osmde/{z}/{x}/{y}.png',
-    };
-  }
-
-  render() {
-    return (
-      <MapView
-        style={styles.mainMap}
-        mapType={MAP_TYPES.NONE}
-        provider={null}
-        region={{
-          latitude: 40.113918,
-          longitude: -88.224916,
-          latitudeDelta: 0.1,
-          longitudeDelta: 0.1,
-        }}>
-        <UrlTile urlTemplate={this.state.apiServerURL}/>
-        <MapView.Marker
-          coordinate={{
-            latitude: 40.114399,
-            longitude: -88.223961,
-          }}
-          title={'title'}
-          description={'description'}
-        />
-      </MapView>
-    );
-  }
-}
 
 /**
  * WIP, should return a Component that only renders the pedway
---
+++ pedwayApp/components/GroundMapView/GroundMapView.js
@@ -0,0 +1,38 @@
+import React, {Component} from 'react';
+import styles from './styles';
+import MapView, {MAP_TYPES, UrlTile} from 'react-native-maps';
+
+/**
+ * Renders a MapView that display the ground level map
+ * we are setting provider to null and UrlTile to OpenStreetMap's API
+ * to use OSM
+ */
+export default class GroundMapView extends React.Component {
+
+  constructor() {
+    super();
+    this.state = {
+      apiServerURL: 'http://a.tile.openstreetmap.de/tiles/osmde/{z}/{x}/{y}.png',
+    };
+  }
+
+  render() {
+    return (
+      <MapView
+        style={styles.mainMap}
+        mapType={MAP_TYPES.NONE}
+        provider={null}
+        region={this.props.region}>
+        <UrlTile urlTemplate={this.state.apiServerURL}/>
+        <MapView.Marker
+          coordinate={{
+            latitude: 40.114399,
+            longitude: -88.223961,
+          }}
+          title={'title'}
+          description={'description'}
+        />
+      </MapView>
+    );
+  }
+}
