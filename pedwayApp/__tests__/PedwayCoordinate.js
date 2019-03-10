b72d91849d7b296da4f042773ab70fcb485db30b
Yilin Ma <yilinma3@illinois.edu>
2019-03-10 16:34:14 -0500

    fixing floating coversion in test

--- pedwayApp/__tests__/PedwayCoordinate.js
+++ pedwayApp/__tests__/PedwayCoordinate.js
@@ -16,14 +16,14 @@ test('Check Coordinate Constructor', () => {
 
 test('Check Coordinate Getter', () => {
   let testVar = new PedwayCoordinate(42, 42);
-  expect(testVar.getLatitude()).toBeCloseTo(42);
-  expect(testVar.getLongitude()).toBeCloseTo(42);
+  expect(testVar.getLatitude()).toBe(42);
+  expect(testVar.getLongitude()).toBe(42);
   testVar = new PedwayCoordinate(0, 0.1);
   expect(testVar.getLatitude()).toBeCloseTo(0);
   expect(testVar.getLongitude()).toBeCloseTo(0.1);
   testVar = new PedwayCoordinate(180, -180);
-  expect(testVar.getLatitude()).toBeCloseTo(180);
-  expect(testVar.getLongitude()).toBeCloseTo(-180);
+  expect(testVar.getLatitude()).toBe(180);
+  expect(testVar.getLongitude()).toBe(-180);
   testVar = new PedwayCoordinate(-70.20232, 42.353523);
   expect(testVar.getLatitude()).toBeCloseTo(-70.20232);
   expect(testVar.getLongitude()).toBeCloseTo(42.353523);
@@ -31,8 +31,8 @@ test('Check Coordinate Getter', () => {
 
 test('Check Coordinate Setter', () => {
   const testVar = new PedwayCoordinate(42, 42);
-  expect(testVar.getLatitude()).toBeCloseTo(42);
-  expect(testVar.getLongitude()).toBeCloseTo(42);
+  expect(testVar.getLatitude()).toBe(42);
+  expect(testVar.getLongitude()).toBe(42);
   testVar.setCoordinates(-70.20232, 42.353523);
   expect(testVar.getLatitude()).toBeCloseTo(-70.20232);
   expect(testVar.getLongitude()).toBeCloseTo(42.353523);
