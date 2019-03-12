cfd67077579555f0df2de6865060d89e89a16d30
Alex Vanyo <vanyoalex@gmail.com>
2019-03-12 14:23:43 -0500

    Made in memory database tests asyncronous (#51)

--- backend/test/entranceController.test.js
+++ backend/test/entranceController.test.js
@@ -24,28 +24,26 @@ afterAll(async () => {
 });
 
 describe('Test the root of the entrance api', () => {
-  test('the default method should be empty', (done) => {
-    request(app).get('/api/pedway/entrance').then((response) => {
+  test('the default method should be empty', async () => {
+    await request(app).get('/api/pedway/entrance').then((response) => {
       expect(response.statusCode).toBe(200);
       expect(Array.isArray(response.body)).toBe(true);
       expect(response.body.length).toBe(0);
-      done();
     });
   });
 
-  test('adding an entrance should return a single entrance', (done) => {
-    new EntranceSchema({
+  test('adding an entrance should return a single entrance', async () => {
+    await new EntranceSchema({
       'type': 'Feature',
       'properties': {'@id': 'node/1469254509', 'entrance': 'yes'},
       'geometry': {'type': 'Point', 'coordinates': [-87.6306729, 41.8862637]},
       'id': 'node/1469254509',
     }).save();
 
-    request(app).get('/api/pedway/entrance').then((response) => {
+    await request(app).get('/api/pedway/entrance').then((response) => {
       expect(response.statusCode).toBe(200);
       expect(Array.isArray(response.body)).toBe(true);
       expect(response.body.length).toBe(1);
-      done();
     });
   });
 });
