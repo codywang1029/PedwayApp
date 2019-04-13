f913330dba065d82105610fa581c192a6778364f
Alex Vanyo <vanyoalex@gmail.com>
2019-04-12 19:22:52 -0500

    Move Status into Entrance (#71)
    * Finished basic implementation
    
    * Fixed linter errors
    
    * Updating to fix PR
    
    * Lint fix
    
    * Added status to section controller
    
    * Added more tests
    
    * Added paramterized tests
    
    * Removed old status model, controller and routes
    
    * Updating to new feedback api
    
    * Move status from section to entrance
    
    * Refactored database test to use the new MongooseEnvironment
    
    * Filled out entrance api and moved status to entrance
    
    * Removed commented out code from authController
    
    * Added comments to tests
    
    * Added non-happy path handlers and tests
    
    * Added more unhappy path tests
    
    * Fix linting
    
    * Added exec to create atrue Promise

--- backend/api/models/entranceModel.js
+++ backend/api/models/entranceModel.js
@@ -7,9 +7,15 @@ const EntranceSchema = new Schema({
   id: {
     type: String,
     required: 'The OSM id',
+    unique: true,
   },
   geometry: mongoose.Schema.Types.Geometry,
   Created_date: {type: Date, default: Date.now},
+  status: {
+    type: String,
+    enum: ['closed', 'dirty', 'closing', 'open'],
+    default: 'open',
+  },
 });
 
 module.exports = mongoose.model('entrance', EntranceSchema);
--- backend/api/routes/sectionRoutes.js
+++ backend/api/routes/sectionRoutes.js
@@ -2,5 +2,11 @@
 module.exports = function(app) {
   const sectionController = require('../controllers/sectionController');
 
-  app.route('/api/pedway/section').get(sectionController.getAll);
+  app.route('/api/pedway/section')
+      .get(sectionController.getAll)
+      .post(sectionController.create);
+
+  app.route('/api/pedway/section/:sectionId')
+      .get(sectionController.getById)
+      .post(sectionController.update);
 };
